71dd247fcabdd2a876f57e8e2294792fd6c99cb4
GBTC408004UR <GBTC408004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-20 10:01:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n********");
+        System.out.println("* \t   *");
+        System.out.println("* \t   *");
+        System.out.println("* \t   *");
+        System.out.println("* \t   *");
+        System.out.println("********");
+        System.out.println("\n  ***");
+        System.out.println(" *   *");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println(" *   *");
+        System.out.println("  ***");
+
+        System.out.println("\n   * ");
+        System.out.println("  * * ");
+        System.out.println(" *   * ");
+        System.out.println("  * * ");
+        System.out.println("   * ");
+
+    }
+}
